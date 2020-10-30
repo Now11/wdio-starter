389ea929333e1d6ae04863b86c727bd67ae73e1e
Now11 <lightonw22@gmail.com>
2020-10-30 18:24:45 +0200

    add: examples, split folders

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
-export * from './base.fragment';
-export * from './base.page';
-export * from './elements';
-export * from './element_utils';
-export * from './browser';
-export * from './report';
+export { BaseFragment } from './fragments';
+export { BasePage } from './pages';
+export { ArrayElement, InputElement, TextElement, ButtonElement } from './elements';
+export { wait } from './element_utils';
+export { browserInterface } from './browser';
+export { step, Log } from './report';
