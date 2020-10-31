9c20216619ef6084f329636ff6b9d322c751c1d1
Now11 <lightonw22@gmail.com>
2020-10-31 23:26:16 +0200

    f

--- src/config/wdio.pipeline.config.ts
+++ src/config/wdio.pipeline.config.ts
@@ -7,7 +7,6 @@ const OUTPUT_DIR = path.join(ROOT_DIR, 'output');
 const SPECS = path.join(ROOT_DIR, 'specs/**/*.spec.ts');
 
 const config: Config = {
-	//runner: 'local',
 	path: '/wd/hub',
 	port: 4444,
 	hostname: 'localhost',
